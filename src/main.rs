52886c4c65c56aa3481ef7140bdf9e93bdf6830b
neo <chojs990222@gmail.com>
2026-06-07 10:49:36 +0900

    feat: Add version flag

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use concord::{App, Result};
 async fn main() -> Result<()> {
     install_rustls_crypto_provider();
 
+    if std::env::args().nth(1).as_deref() == Some("--version") {
+        println!("concord {}", env!("CARGO_PKG_VERSION"));
+        return Ok(());
+    }
+
     let app = App::new();
     app.run().await
 }
