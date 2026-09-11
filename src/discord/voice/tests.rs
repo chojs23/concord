d084704bb1bffa1fd114065fe6a593fe745441a5
Neo <chojs990222@gmail.com>
2026-09-11 21:44:09 +0900

    fix(voice): Use the host-default buffer for all microphone input streams (#356)

--- src/discord/voice/tests.rs
+++ src/discord/voice/tests.rs
@@ -2363,16 +2363,8 @@ fn voice_input_config_prefers_mono_then_sample_format() {
 
 #[cfg(feature = "voice-playback")]
 #[test]
-fn voice_input_buffer_size_requests_small_supported_fixed_buffer() {
-    let supported = cpal::SupportedBufferSize::Range { min: 128, max: 960 };
-    assert_eq!(
-        voice_input_buffer_size(&supported),
-        cpal::BufferSize::Fixed(VOICE_MIC_PREFERRED_BUFFER_FRAMES)
-    );
-    assert_eq!(
-        voice_input_buffer_size(&cpal::SupportedBufferSize::Unknown),
-        cpal::BufferSize::Default
-    );
+fn voice_input_buffer_size_uses_host_default() {
+    assert_eq!(voice_input_buffer_size(), cpal::BufferSize::Default);
 }
 
 #[cfg(feature = "voice-playback")]
